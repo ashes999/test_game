fa6a8eec5c552bd533f9e1d75aae3e406ecd3f6b
ashes999 <ashes999@yahoo.com>
2016-07-19 00:16:10 -0400

    Added images.

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ Jabal.init(800, 600)
 Jabal.load_audio(["blip.wav"])
 
 s = AwesomeMath.get_awesomeness()
-e = Entity().size(s, s).colour('red').move(32, 16).move_with_keyboard()
+e = Entity().size(s, s).colour('red').move(32, 16).move_with_keyboard().image('assets/images/avatar.png')
 e.on_click(lambda: Jabal.play_audio("blip"))
