5e56f3f288218cd50743c85be38a017d8bdb8b59
ashes999 <ashes999@yahoo.com>
2016-07-22 01:18:32 -0400

    Final, working version.

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from browser import console
 import sys
 import jabal
 
-console.log("Running Python v{0}".format(sys.version))
+console.log("Running Python v{0}!".format(sys.version))
 
+def play_blip():
+    jabal.play_audio("blip")
+
+# Generates a JS error, see: https://github.com/brython-dev/brython/issues/458
 from awesome import awesome_math
+from awesome.bestest import magic
 
 # fearsome.py
 import fearsome
@@ -14,12 +19,16 @@ jabal.init(800, 600)
 jabal.load_audio(["blip.wav"])
 
 s = awesome_math.get_awesomeness()
-print("S is {0}".format(s))
+m = magic.magic_number() + 1
 
-e = jabal.Entity().size(s, s).move(32, 16).move_with_keyboard().image('assets/images/avatar.png')
-e.on_click(lambda: jabal.play_audio("blip"))
+print("S is {0}".format(s))
+print("Magic is {0}".format(m))
+
+e = jabal.Entity().size(48, 48).move(32, 16).move_with_keyboard().colour('red')
+e.on_click(play_blip)
 
 # import awesome.randme doesn't work
 # import randme does
 import randme
 console.log(randme.randit())
+
