771430a12c67a7c76534dc284695e8e90ef9afe1
ashes999 <ashes999@yahoo.com>
2016-07-22 23:35:23 -0400

    Turn off background audio

--- main.py
+++ main.py
@@ -4,33 +4,9 @@ import jabal
 
 console.log("Running Python v{0}!".format(sys.version))
 
-# Generates a JS error, see: https://github.com/brython-dev/brython/issues/458
-from awesome import awesome_math
-from awesome.bestest import magic
+#jabal.init(800, 600)
+#jabal.load_audio(["blip.lol"])
 
-# fearsome.py
-import fearsome
-console.log(fearsome.say_hi())
-
-jabal.init(800, 600)
-jabal.load_audio(["blip.lol"])
-
-s = awesome_math.get_awesomeness()
-m = magic.magic_number() + 1
-
-print("S is {0}".format(s))
-print("Magic is {0}".format(m))
-
-e = jabal.Entity().size(48, 48).move(32, 16).move_with_keyboard().colour('red')
-
-def play_blip(t):
-    jabal.play_audio("blip")  
-    e.colour('blue')
-    
-e.on_click(play_blip)
-
-# import awesome.randme doesn't work
-# import randme does
-import randme
-console.log(randme.randit())
-
+#e = jabal.Entity().size(48, 48).move(32, 16).move_with_keyboard().colour('red')
+  
+#e.on_click(lambda: jabal.play_audio("blip"))
